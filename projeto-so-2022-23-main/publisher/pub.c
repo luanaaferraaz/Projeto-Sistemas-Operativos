eb0a063f98a51122fb77452f980e13acd583a247
luanaaferraaz <109438076+luanaaferraaz@users.noreply.github.com>
2023-01-14 10:54:35 +0000

    pub done i guess

--- projeto-so-2022-23-main/publisher/pub.c
+++ projeto-so-2022-23-main/publisher/pub.c
@@ -14,7 +14,12 @@
 
 char pub_pipe_name[MAX_CLIENT_PIPE_NAME];
 char box_name[MAX_BOX_NAME];
+char *msg_from_pub = "9"; 
 
+// check if user entered EOF in stdin
+// gets the first character from the input and checks if it is EOF,
+// if it isn't EOF puts the charater back because it may be part of the
+// message the pub wrote.
 int check_for_EOF() {
     if (feof(stdin)) return 1;
     int c = getc(stdin);
@@ -24,7 +29,6 @@ int check_for_EOF() {
 }
 
 void send_message_to_mb(int pub_pipe, char *message){
-    //talvez dar um sinal para a thread no mbroker ler
 
   size_t len = strlen(message);
   size_t written = 0;
@@ -37,35 +41,42 @@ void send_message_to_mb(int pub_pipe, char *message){
       }
       written += (size_t)ret;
   }
-  //talvez um sinal para o mbroker a tratar disto escrever a 
-  //mensagem ler da pipe e escrever no ficheiro no tfs
+
 }
 
 void wait_for_messages(){ // wait for input messages
     char *reading = NULL;
     int pub_pipe = 0;
     bool wrote_message = false;
+    //while it doesn´t read EOF from the input, runs
     while(!check_for_EOF()){
         size_t len = 0;
         ssize_t lineSize = 0;
+        //gets a line
         lineSize = getline(&reading, &len, stdin);
         if(lineSize > 0) {
-            printf("got your message: %s\n", reading);
             char message[1024]="";
-            strcat(message, "9");
+           
+           //concatenates the message in a buffer with sending message code - 9
+            strcat(message, msg_from_pub);
             strcat(message, "|");
             strcat(message, reading);
-            pub_pipe = open(pub_pipe_name, O_WRONLY); //TO DO é preciso alguem à espera de read desta pipe, a thread
-            if(pub_pipe==-1){
-                fprintf(stderr, "Failed to open--: %s\n", strerror(errno));
-                exit(EXIT_FAILURE);
+            
+            //if it´s the first time writing to the pipe, needs to open it
+            if(!wrote_message){
+                pub_pipe = open(pub_pipe_name, O_WRONLY); //TO DO é preciso alguem à espera de read desta pipe, a thread
+                if(pub_pipe==-1){
+                    fprintf(stderr, "Failed to open: %s\n", strerror(errno));
+                    exit(EXIT_FAILURE);
+                }
             }
             wrote_message = true;
             send_message_to_mb(pub_pipe, message);
             free(reading);
         }
     }
-    if(wrote_message  && close(pub_pipe)==-1) { // EOF closing session
+    // EOF closing session, need to close the pipe if it was opened
+    if(wrote_message  && close(pub_pipe)==-1) { 
         fprintf(stderr,"Failed to close pipe(%s): %s\n", pub_pipe_name,
                 strerror(errno));
         exit(EXIT_FAILURE);  
@@ -87,8 +98,12 @@ int main(int argc, char **argv) {
         strcpy(register_pipe, argv[1]);
         strcpy(pub_pipe_name, argv[2]); //client_pipe_name
         strcpy(box_name, argv[3]);
+
+        //writes the request (for the pub to connect to the box) in server_pipe
         send_request(PUBLISHER, register_pipe, pub_pipe_name, box_name);
-        if(check_connected(pub_pipe_name)==-1){ //check if sub connected to box specified in input
+        
+        //check if sub connected to box specified in input
+        if(check_connected(pub_pipe_name)==-1){ 
             exit(EXIT_FAILURE);
         }
         wait_for_messages();
